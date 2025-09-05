284d322e8c09d1c512e5ef5bb47b54a68c54abd8
Maksim Kleverov <klevermx@gmail.com>
2025-09-05 18:04:01 -0500

    read env for npm

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
-import { defineConfig } from "vite";
+
+import { defineConfig, loadEnv } from "vite"
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig({
-  base: process.env.HUMAN_AGING_VITE_BASE_URL || "/",   // ← no hardcode; set per env
-  server: { host: "::", port: 8080 },
-  plugins: [react()],
-  resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
+export default defineConfig(({ mode }) => {
+  // Load .env, .env.local, .env.[mode], .env.[mode].local
+  const env = loadEnv(mode, process.cwd(), ""); // no third arg => load all keys
+
+  return {
+    base: env.VITE_HUMAN_AGING_BASE_URL || "/",   // <- pulled from .env files
+    server: { host: "::", port: 8080 },
+    plugins: [react()],
+    resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
+  };
 });
 
 
