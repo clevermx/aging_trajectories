6741e722f39b48e231bc064d4c9d3c0d42d10526
Maksim Kleverov <klevermx@gmail.com>
2026-03-13 15:28:14 -0500

    add average expression links

--- src/components/PopulationData.tsx
+++ src/components/PopulationData.tsx
@@ -368,7 +368,7 @@ export const PopulationCard = ({
                 rel="noopener noreferrer"
                 className="px-5 py-2 rounded-lg bg-white/50 hover:bg-white/40 transition text-sm font-medium"
               >
-                Pseudobulk
+                Average expression
               </a>
             )}
           </div>
